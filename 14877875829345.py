731f001e7b5d1b6d06b878304fd2116aab152c9b
Amiralikarim <82859448+Amiralikarim@users.noreply.github.com>
2021-04-20 09:42:26 +0430

    Add files via upload

---
+++ 14877875829345.py
@@ -0,0 +1,10 @@
+# My name is Amir Ali Karimi 
+# This app is about determining the leap year of the solar year
+year=int(input("sal shamsi ra baray taiin kabise bodan ya naboodan vared konid"":"))
+if (year%33 == 1) or (year%33 == 5)\
+      or(year%33 == 9) or (year%33 == 13)\
+            or (year%33 == 17) or (year%33 == 22)\
+                 or (year%33 == 26) or (year%33 == 30) :
+                 print("sal entekhab shode kabise ast")
+else:
+      print("sal entekhab shode kabise nist")
